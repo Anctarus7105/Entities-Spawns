67b3ed1e386b5022c19ceaed06ae539e0baab4cb
Anctarus7105 <idzhabkin@gmail.com>
2025-12-17 19:10:50 +0400

    Create NerdSpawner.lua

---
+++ NerdSpawner.lua
@@ -0,0 +1,6 @@
+local chance = math.random(1, 2)
+wait(math.random(100, 110))
+if chance == 1 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/Nerd.lua"))()
+if chance == 2 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/StayingNerd.lua"))()
