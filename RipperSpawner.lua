c795f875871526087a538877a5bc644a8de836a4
Anctarus7105 <idzhabkin@gmail.com>
2026-01-11 17:34:01 +0400

    Update RipperSpawner.lua

--- RipperSpawner.lua
+++ RipperSpawner.lua
@@ -15,7 +15,7 @@ TW:Play()
 local entity = spawner.Create({
 	Entity = {
 		Name = "Ripper",
-		Asset = "rbxassetid:////12234498940",
+		Asset = "rbxassetid:////12262768551",
 		HeightOffset = 0
 	},
 	Lights = {
@@ -53,13 +53,13 @@ local entity = spawner.Create({
 	},
 	Crucifixion = {
 		Enabled = true,
-		Range = 40,
+		Range = 400,
 		Resist = false,
 		Break = true
 	},
 	Death = {
 		Type = "Guiding", -- "Curious"
-		Hints = {"You died who you call Ripper...", "When lights color red HIDE!", "I hope you follow the guide.", "Remember it all."},
+		Hints = {"You died who you call Ripper...", "You can tell his presence by the lights and his scream.", "Hide when does this!", ""},
 		Cause = "Ripper"
 	}
 })
@@ -72,6 +72,39 @@ end)
 
 entity:SetCallback("OnStartMoving", function()
     print("Entity has started moving")
+function GitAud(soundgit,filename)
+
+    SoundName=tostring(SoundName)
+
+    local url=soundgit
+
+    local FileName = filename
+
+    writefile(FileName..".mp3", game:HttpGet(url))
+
+    return (getcustomasset or getsynasset)(FileName..".mp3")
+
+end
+
+function CustomGitSound(soundlink, vol, filename)
+
+    local sound = Instance.new("Sound")
+
+    sound.SoundId = GitAud(soundlink, filename)
+
+    sound.Parent = workspace
+
+    sound.Name = "rippermoving"
+
+    sound.Volume = 10
+
+    sound.PlaybackSpeed = 1
+
+   sound:Play()
+
+end
+
+CustomGitSound("https://github.com/Kotyara19k-Doorsspawner/Random-files/raw/main/Y2meta.app%20-%20Ripper%20Has%20Moving%20Sound%20(320%20kbps).mp3", 1, "rippermoving")
 end)
 
 entity:SetCallback("OnEnterRoom", function(room, firstTime)
@@ -105,13 +138,103 @@ end)
 entity:SetCallback("OnDespawned", function()
     print("Entity has despawned")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Custom-Badges/refs/heads/main/Hardcore%20Mode/Ripper.lua"))()
+
+	else
+		
+local Scream = Instance.new("Sound")	
+Scream.Parent = Workspace
+Scream.SoundId = "rbxassetid://1837829565"
+Scream.Volume = 9.9
+Scream.Looped = false
+Scream.Pitch = 1
+Scream:Play()
 end)
 
 entity:SetCallback("OnDamagePlayer", function(newHealth)
 	if newHealth == 0 then
 		print("Entity has killed the player")
 	else
-		print("Entity has damaged the player")
+		workspace.RipeMoving:Stop()
+workspace.Ripper:Destroy()
+
+local player = game.Players.LocalPlayer
+
+player.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
+player.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Physics, false)
+player.Character.HumanoidRootPart.Anchored = true
+
+function GitAud(soundgit,filename)
+    SoundName=tostring(SoundName)
+    local url=soundgit
+    local FileName = filename
+    writefile(FileName..".mp3", game:HttpGet(url))
+    return (getcustomasset or getsynasset)(FileName..".mp3")
+end
+
+function CustomGitSound(soundlink, vol, filename)
+    local Sound = Instance.new("Sound")
+    Sound.SoundId = GitAud(soundlink, filename)
+    Sound.Parent = workspace
+    Sound.Volume = vol
+    Sound:Play()
+end
+
+CustomGitSound("https://github.com/Kotyara19k-Doorsspawner/Random-files/raw/main/Y2meta.app%20-%20Doors%20HardCore%20Mode_%201%20ripper%20part%20sound%20(320%20kbps).mp3", 1, "Ripper5Sound")
+wait(1.5)
+function GitAud(soundgit,filename)
+    SoundName=tostring(SoundName)
+    local url=soundgit
+    local FileName = filename
+    writefile(FileName..".mp3", game:HttpGet(url))
+    return (getcustomasset or getsynasset)(FileName..".mp3")
+end
+
+function CustomGitSound(soundlink, vol, filename)
+    local Sound1 = Instance.new("Sound")
+    Sound1.SoundId = GitAud(soundlink, filename)
+    Sound1.Parent = workspace
+    Sound1.Volume = vol
+    Sound1:Play()
+end
+
+CustomGitSound("https://github.com/Kotyara19k-Doorsspawner/Random-files/raw/main/Y2meta.app%20-%20Doors%20Hardcore%20Mode_%20Ripperscare%20part%202%20sound%20(320%20kbps).mp3", 1, "Ripper6Sound")
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Image = "rbxassetid://236542974"
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = true
+wait(0.3)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = false
+wait(0.1)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = true
+wait(0.2)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = true
+wait(0.2)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = false
+wait(0.3)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = true
+wait(0.2)
+game.Players.LocalPlayer.PlayerGui.MainUI.Drip.Visible = false
+wait(0.20)
+local player = game.Players.LocalPlayer
+local humanoid = player.Character:FindFirstChild("Humanoid")
+humanoid.Health = humanoid.Health - 100
+game:GetService("ReplicatedStorage").GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Ripper"
+local func, setupval, getinfo, typeof, getgc, next = nil, debug.setupvalue or setupvalue, debug.getinfo or getinfo, typeof, getgc, next
+
+for i,v in next, getgc(false) do
+    if typeof(v) == "function" then
+        local info = getinfo(v)
+        if info.currentline == 54 and info.nups == 2 and info.is_vararg == 0 then
+            func = v
+            break
+        end
+    end
+end
+
+local function DeathHint(hints, type: string)
+    setupval(func, 1, hints)
+    if type ~= nil then
+        setupval(func, 2, type)
+    end
+end
 	end
 end)
 
